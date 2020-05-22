81da140bdd496889660634349830d0003c518995
Saif Amer <seif.ealaan@gmail.com>
2020-05-22 20:43:52 +0200

    advancednaql

---
+++ NaqlScreen/lib/Splash.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import './Screen2.dart';
+
+class Splash extends StatelessWidget {
+  Splash({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[
+          // Adobe XD layer: 'BG' (shape)
+          Container(
+            width: 414.0,
+            height: 896.0,
+            decoration: BoxDecoration(
+              color: const Color(0xffffffff),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(46.0, 302.0),
+            child:
+                // Adobe XD layer: 'logo' (component)
+                Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
